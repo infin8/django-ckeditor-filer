79ead45659027c08cfaaa469a8fcce0ea1b69291
infin8 <nathangkc@gmail.com>
2015-06-30 14:50:27 +0700

    Update __init__.py

--- ckeditor_filer/__init__.py
+++ ckeditor_filer/__init__.py
@@ -1,7 +1,8 @@
+import fields, views, urls, models, widgets
+
 try:
     from django.template.loader import add_to_builtins
 except:
     from django.template.base import add_to_builtins
-
 
 add_to_builtins('ckeditor_filer.templatetags.ckeditor_tags')
